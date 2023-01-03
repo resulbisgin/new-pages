99ded0da08ad3403564628a32569e59fd5c44721
resulbisgin <resulbiginresul_20011@hotmail.com>
2023-01-03 16:43:02 +0300

    add linux.svg

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import avatar from './images/avatar.png'
 import patika from "./images/newPatikaLogo.svg"
 import Slider from "react-slick";
-import html from "./images/html.svg"
-import css from "./images/css.svg"
-import js from "./images/javascript.svg"
-import react from "./images/react.svg"
-import next from "./images/next.svg"
-import bootstrap from "./images/bootstrap.svg"
-import tailwind from "./images/tailwind.svg"
-import php from "./images/php.svg"
+import {data} from "./data"
 import {AiFillMail,AiFillLinkedin} from 'react-icons/ai'
 
 function App() {
@@ -52,49 +45,7 @@ function App() {
   };
 
 
-  const data=[
-    {
-        id:1,
-        title:'HTML',
-        url:html
-    },
-    {
-        id:2,
-        title:'CSS',
-        url:css
-    },
-    {
-        id:3,
-        title:'Javascript',
-        url:js
-    },
-    {
-        id:4,
-        title:'React',
-        url:react
-    },
-    {
-        id:5,
-        title:'Next',
-        url:next
-    },
-    {
-        id:6,
-        title:'Bootstrap',
-        url:bootstrap
-    },
-    {
-        id:7,
-        title:'Tailwind',
-        url:tailwind
-    },
-    {
-        id:8,
-        title:'PHP',
-        url:php
-    }
 
-]
 
   return (
     <>
---
+++ src/data.js
@@ -0,0 +1,57 @@
+import html from "./images/html.svg"
+import css from "./images/css.svg"
+import js from "./images/javascript.svg"
+import react from "./images/react.svg"
+import next from "./images/next.svg"
+import bootstrap from "./images/bootstrap.svg"
+import tailwind from "./images/tailwind.svg"
+import php from "./images/php.svg"
+import linux from "./images/linux.svg"
+export const data=[
+    {
+        id:1,
+        title:'HTML',
+        url:html
+    },
+    {
+        id:2,
+        title:'CSS',
+        url:css
+    },
+    {
+        id:3,
+        title:'Javascript',
+        url:js
+    },
+    {
+        id:4,
+        title:'React',
+        url:react
+    },
+    {
+        id:5,
+        title:'Next',
+        url:next
+    },
+    {
+        id:6,
+        title:'Bootstrap',
+        url:bootstrap
+    },
+    {
+        id:7,
+        title:'Tailwind',
+        url:tailwind
+    },
+    {
+        id:8,
+        title:'PHP',
+        url:php
+    },
+    {
+        id:9,
+        title:'Linux',
+        url:linux
+    }
+
+]
